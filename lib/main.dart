2c3c49f6b60a8f917597f114ebf5d0dba5fd1f72
Pierre B <pierreburnier07@gmail.com>
2025-10-01 01:53:12 +0200

    ajout du home.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app2/home.dart';
 import 'package:flutter/material.dart';
 import 'package:app2/mytheme.dart';
 
@@ -9,9 +10,6 @@ class MyApp2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = MyTheme.dark();
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: theme,
-      home: Home(),
-    );
+    return MaterialApp(title: 'Flutter Demo', theme: theme, home: Home());
+  }
 }
