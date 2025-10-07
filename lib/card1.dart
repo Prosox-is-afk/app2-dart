9e97010960bd722b635fe3771c2a9e788d020ee1
Pierre B <pierreburnier07@gmail.com>
2025-10-08 01:08:52 +0200

    ajout du detail et task + ajout code card 1

--- lib/card1.dart
+++ lib/card1.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
+import '../models/task.dart';
+import 'detail.dart';
 
 class Ecran1 extends StatelessWidget {
+  final List<Task> myTasks = Task.generateTask(6);
+
+  Ecran1({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blueGrey,
-      child: const Center(child: Text('Ecran 1')),
+    return ListView.builder(
+      itemCount: myTasks.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Card(
+          color: Colors.white,
+          elevation: 7,
+          margin: const EdgeInsets.all(10),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.lightBlue,
+              child: Text(myTasks[index].id.toString()),
+            ),
+            title: Text(myTasks[index].title),
+            subtitle: Text(myTasks[index].tags.join(" ")),
+            trailing: IconButton(
+              icon: const Icon(Icons.edit),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Detail(task: myTasks[index]),
+                  ),
+                );
+              },
+            ),
+          ),
+        );
+      },
     );
   }
 }
